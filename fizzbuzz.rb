e5353fc0798044b52ae9589d8b28477ec9162070
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-04 03:56:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  if num % 3 == r
+  if (num % 3) == (num % 3).round
+    return "Fizz"
+  elsif (num % 5) == (num % 5).round
     return
-  elsif num
-    return
+  elsif (num % 5) ==
   else
     return "nil"
   end
